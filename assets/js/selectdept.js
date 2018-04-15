8bfc6397caed2c575a8be9d5b83f312a53471321
farmingPotatoes <tomknighton@icloud.com>
2018-04-15 22:23:59 +0100

    First commit

---
+++ assets/js/selectdept.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+function selectLSPD() {
+	
+}
+function selectBCSO() {
+	
+}
+function selectSAHP() {
+	
+}
+function selectCIV() {
+	
+}
+function selectCOMM() {
+	
+}
+function selectEMS() {
+	
+}

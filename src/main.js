4657d53594122aa09229eb59c9f4cb4ab5b70c24
Tom K <tomknighton@icloud.com>
2020-10-13 08:09:35 +0100

    fix for routing

--- src/main.js
+++ src/main.js
@@ -22,7 +22,20 @@ Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
-const routes = [{ path: "/", component: HomePage, name: "home" }, { path: "/computerdating", component: ComputerDating, name: "computerdating" }, { path: "/rules", component: RulesPage, name: "rules" }, { path: "/thankyou", component: ThankYou, name: "thankyou" }];
+const routes = [{
+    path: "/",
+    component: HomePage,
+    name: "home",
+    beforeEnter: (to, from, next) => {
+        const { uri } = to.query;
+        if (uri != null && uri != '/') {
+            next(false);
+            router.push(uri);
+        } else {
+            next();
+        }
+    }
+}, { path: "/computerdating", component: ComputerDating, name: "computerdating" }, { path: "/rules", component: RulesPage, name: "rules" }, { path: "/thankyou", component: ThankYou, name: "thankyou" }];
 const router = new VueRouter({
     mode: 'history',
     routes // short for `routes: routes`

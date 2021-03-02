8b3da23ec4d8eb7e7c50f2a076aa66466d8ef9d1
Tom K <tomknighton@icloud.com>
2021-03-02 17:52:25 +0000

    whats new 4.0

---
+++ src/components/WhatsNew4.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="bg">
+    <section class="about section">
+      <div class="section-inner">
+        <h2 class="heading"><strong> Gary Portal 4.0</strong></h2>
+        <div class="content">
+          <p>
+            Gary Portal 4.0 includes numerous AMAZING features, such as another
+            complete rewrite of the app (retaining all things Gary)
+          </p>
+          <p>What else, you beg? Have a taste:</p>
+        </div>
+        <!--//content-->
+      </div>
+      <!--//section-inner-->
+    </section>
+
+    <section class="about section">
+      <div class="section-inner">
+        <div class="content">
+          <ul style="list-style: none">
+            <li>
+                <hr>
+                <strong><h3 class="heading">- All New app!</h3></strong>
+                <p>The app has been completely rewritten from the ground up, it's now good!</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Turn out the Lights</h3></strong>
+                <p>The app now supports dark mode (two years later), Rest your eyes after a hard day of Garrying</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Post or don't</h3></strong>
+                <p>It took a lot of sweating from Murray himself but the feed section has been smushed together, all our favourite posts are now together in one place rather than scattered between a posts, polls and adit logs section.</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- HD in high definition</h3></strong>
+                <p>There's a new camera, and you can draw on it, you are your own canvas</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- We need to talk</h3></strong>
+                <p>An all new chat! yay! There's a whole new chat section, completely built by Gary this time. And get this, it no longer crashes whenever someone starts typing! Truly, this is a technological achievment that will be written about in the future. There's also some fancy commands you can use if you know how (?), 😘👅</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Slay Queen</h3></strong>
+                <p>Someone got you down in the dumps? :( Someone insulting your mum? Someone asking you to remove your legally built balcony? Luckily for you, you can now GaryBlock someone, removing their posts, messages, adit logs and even physical presence from your world! Of course, when they apologise, you can unblock them at any time, and if you're annoyed but need them too much, you can always report them instead of blocking them completely</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Notifications</h3></strong>
+                <p>There's notifications now</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Power</h3></strong>
+                <p>Staff members and admins now have their own little hidey hole in which they can wield their massive amounts of power, right inside Gary Portal! Staff members can edit users in their own team, and admins can shape the GaryVerse to their will.</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Together but Apart</h3></strong>
+                <p>The app has been rewritten in a new design language and paradigms, what this means for you is that you may have to take a <a href="https://www.youtube.com/watch?v=5HhE1QcStHE">lie-down for a full hour</a> after first seeing it. Beyond that, it means vast improvements in battery life and app size! As well as your overally GaryJoyment.</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- Superior Coding</h3></strong>
+                <p>Wow, the code looked rubbish and it now looks quite good, which I think is an improvement - at the very least for me. Some of the old code used to leak memory all over the place and it turns out it's not meant (or really allowed) to do that, so that was fixed. Sometimes network calls failed too which was weird so those are all fixed, also there was some weird authentication-y things happening in the old version you know? That's fixed too, a lot of stuff was GaryFixed. Some stuff was GaryBroken too, but only because Al needs stuff to do in the next few weeks and months, not because he missed them or ignored them.</p>
+                <hr>
+            </li>
+            <li>
+                <strong><h3 class="heading">- MORE</h3></strong>
+                <p>There's loads more, keep an eye out for the official changelog in the App Store when Gary Portal 4 releases (soon), and We can check if you download it or not, so do.</p>
+                <hr>
+            </li>
+          </ul>
+        </div>
+        <!--//content-->
+      </div>
+      <!--//section-inner-->
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.bg {
+  background-image: url("../assets/img/home-banner.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
+  height: 100%;
+  color: white;
+  padding: 50px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ComputerDating from "./components/ComputerDating.vue";
 import HomePage from "./components/HomePage.vue";
 import RulesPage from "./components/RulesAndRegulations.vue";
 import ThankYou from "./components/FeedbackThankYou.vue";
+import WhatsNew4 from "./components/WhatsNew4.vue";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import VueRouter from 'vue-router'
@@ -30,7 +31,8 @@ const routes = [{
     { path: "/rules",component: RulesPage,name: "rules" },
     { path: "/thankyou",component: ThankYou,name: "thankyou" },
     { path: "*",component: HomePage },
-    { path: "/computerdating",component: ComputerDating,name: "computerdating" },
+    { path: "/computerdating",component: ComputerDating, name: "computerdating" },
+    { path: "/version4", component: WhatsNew4, name: "whatsnew40" }
 ];
 
 const router = new VueRouter({

b2aabbcc572d82e767b7b5536a464ef44d2c000b
Tom K <tomknighton@icloud.com>
2021-06-29 13:46:21 +0100

    feat(profile): Points module, class styling

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ const Home = (props) => {
         <meta name="description" content="Gary Portal Website" />
       </Head>
 
-      <div className="px-5 pt-5 justify-center place-items-center">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="bg-darkgraylight shadow-lg rounded-xl pt-7 pb-5 px-3 col-span-2 py-10">
-            <div className="flex flex-col md:flex-row place-items-center md:place-items-start">
+      <div className="profileContainer">
+        <div className="profileGrid">
+          <div className="profileCard2">
+            <div className="flexrow_sm_grid">
               <img
                 src={user?.userProfileImageUrl}
                 className="rounded-full h-28 w-28 shadow-lg md:h-40 md:w-40"
@@ -25,10 +25,25 @@ const Home = (props) => {
                 <h1 className="text-xl pl-0 md:pl-0.5 italic">@{user?.userName}</h1>
                 <button class="mt-1 md:mt-5 p-3 px-5 text-base font-semibold focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200 rounded-xl block border-b border-purple-300 bg-purple-200 hover:bg-purple-300 text-purple-700">Staff Panel</button>
               </div>
-
             </div>
           </div>
+
+          <div className="profileCard row-span-2">
+            <div className="flex flex-col place-items-center gap-4">
+                <h1 className="text-3xl font-bold">Points:</h1>
+                <hr/>
+
+                <h1 className="profilePointsText">AMIGO POINTS: {user?.userPoints?.amigoPoints?.toLocaleString()}</h1>
+                <h1 className="profilePointsText">POSITIVE POINTS: {user?.userPoints?.positivityPoints?.toLocaleString()}</h1>
+                <br/>
+                <h1 className="profilePointsText">PRAYERS: {user?.userPoints?.prayers?.toLocaleString()}</h1>
+                <h1 className="profilePointsText">MEANINGFUL PRAYERS: {user?.userPoints?.meaningfulPrayers?.toLocaleString()}</h1>
+            </div>
+          </div>
+
+
         </div>
+
       </div>
     </div>
   );
